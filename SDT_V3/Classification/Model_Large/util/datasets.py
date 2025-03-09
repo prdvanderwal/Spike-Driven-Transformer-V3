18e4860144b185cae77e56cf76ef3f781abf051f
bollossom <2223453260@qq.com>
2025-03-09 16:48:54 +0800

    Update datasets.py

--- SDT_V3/Classification/Model_Large/util/datasets.py
+++ SDT_V3/Classification/Model_Large/util/datasets.py
@@ -16,10 +16,14 @@ import torch
 from timm.data import create_transform
 from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 
+
+    
 def build_dataset(is_train, args):
     transform = build_transform(is_train, args)
-    root = os.path.join(args.data_path, 'train' if is_train else 'val')
+
+    root = os.path.join(args.data_path, "train" if is_train else "val")
     dataset = datasets.ImageFolder(root, transform=transform)
+
     return dataset
 
 def build_transform(is_train, args):
