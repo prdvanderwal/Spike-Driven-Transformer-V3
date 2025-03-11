98b66e2e700db9a2e7fd420dc7f6a3a9c4384448
Xuerui Qiu <87843847+bollossom@users.noreply.github.com>
2025-03-11 22:05:53 +0800

    Update coco_instance.py

--- SDT_V3/Detection/configs/_base_/datasets/coco_instance.py
+++ SDT_V3/Detection/configs/_base_/datasets/coco_instance.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/public/liguoqi/coco_yolo/'
+data_root = '../coco_yolo/'
 
 # Example to use different file client
 # Method 1: simply set the data root and let the file I/O module
